e3e5e3cd3e26049a108b7ca24e665903ef377090
wis-vgaydukov <bratstakan@gmail.com>
2017-03-15 21:21:37 +0300

    Beautify conversations list

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+
+const lastConversations = require('../../../mock-api/last-conversations.json');
 
 @Component({
   selector: 'qs-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class MainComponent {
   public user = { name: 'Michael' };
+  public lastConversations = lastConversations;
 
   constructor() { }
 }
